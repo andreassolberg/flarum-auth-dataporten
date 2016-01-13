7462b73f52b97e8228fe6cb50b59e815721c0779
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2016-01-13 13:20:29 +0100

    Fix namespace for AddDataportenAuthRoute.php

--- src/Listener/AddDataportenAuthRoute.php
+++ src/Listener/AddDataportenAuthRoute.php
@@ -8,7 +8,7 @@
  * file that was distributed with this source code.
  */
 
-namespace Flarum\Auth\GitHub\Listener;
+namespace Flarum\Auth\Dataporten\Listener;
 
 use Flarum\Event\ConfigureForumRoutes;
 use Illuminate\Contracts\Events\Dispatcher;
