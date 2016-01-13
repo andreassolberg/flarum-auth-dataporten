ab68ace1369b8ae68b3369635810c563cc202d26
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2016-01-13 13:20:41 +0100

    uncomment code

--- bootstrap.php
+++ bootstrap.php
@@ -1,11 +1,11 @@
 <?php
 
-echo '<pre>';
+// echo '<pre>Hello!';
 
 use Flarum\Auth\Dataporten\Listener;
 use Illuminate\Contracts\Events\Dispatcher;
 
 return function (Dispatcher $events) {
     $events->subscribe(Listener\AddClientAssets::class);
-    // $events->subscribe(Listener\AddDataportenAuthRoute::class);
+    $events->subscribe(Listener\AddDataportenAuthRoute::class);
 };
